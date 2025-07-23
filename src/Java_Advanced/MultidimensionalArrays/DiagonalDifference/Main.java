3ecd843523907808ea51f7a733296d9bb38f22a6
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2025-07-23 14:47:05 +0300

    More exercises

--- src/Java_Advanced/MultidimensionalArrays/DiagonalDifference/Main.java
+++ src/Java_Advanced/MultidimensionalArrays/DiagonalDifference/Main.java
@@ -26,14 +26,6 @@ public class Main {
         }
     }
 
-    private static void printMatrix(int [][]matrix) {
-        for (int row=0;row<matrix.length;row++) {
-            for (int col=0;col<matrix[0].length;col++) {
-                System.out.print(matrix[row][col] +" ");
-            }
-            System.out.println();
-        }
-    }
 
     private static int primaryDiagonal(int [][]matrix) {
         int sum=0;
